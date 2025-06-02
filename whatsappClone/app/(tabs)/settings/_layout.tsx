637b6cb36f070d2415cf62a3e9a47d52d00ebecf
Eli-Keli <muthokaelikeli@gmail.com>
2025-06-02 18:16:36 +0300

    01 Settings Screen

---
+++ whatsappClone/app/(tabs)/settings/_layout.tsx
@@ -0,0 +1,24 @@
+import Colors from '@/constants/Colors';
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    title: 'Settings',
+                    headerLargeTitle: true, // Only available on iOS
+                    headerShadowVisible: false,
+                    headerStyle: { backgroundColor: Colors.background },
+
+                    headerSearchBarOptions: {
+                        placeholder: 'Search',
+                    },
+                }}
+            />
+        </Stack>
+    );
+};
+
+export default Layout;
